c2ea4fc737c3fa96fff958f61e46aae275b4e049
Unknown <anaantunes@cpejr.com.br>
2019-03-20 11:08:42 -0300

    Página de Login e Rotas
    Fiz a página de login, mais ainda preciso deixar ela bonita,  fiz as rotas pra pagina de login e pra pagina user, criei a pagina user (fala se foi logado ou nao), coloquei meu email no firebase. Só falta editar o arquivo .env

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const firebase = require('firebase');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -10,5 +11,31 @@ router.get('/queue', (req, res) => {
   res.render('queue', {title:'Queue'});
 });
 
+router.get('/login', (req, res) => {
+  res.render('login', {title:'Login'});
+});
+
+router.get('/user', function(req, res, next) {
+  res.render('user', {title:'User'});
+});
+
+/**
+ * POST LOGIN
+ */
+
+router.post('/login',(req,res)=> {
+  const user = req.body.user;
+
+  firebase.auth().signInWithEmailAndPassword(user.email, user.password).then((userID) => {
+    
+  /* console.log(userID);*/
+   res.redirect('/user');
+ }).catch(function(error) {
+   // Handle Errors here.
+   var errorCode = error.code;
+   var errorMessage = error.message;
+   // ...
+ });
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -9,4 +9,13 @@ router.get('/', function(req, res, next) {
 router.get('/queue', function(req, res, next) {
   res.render('queue', {title:'queue'});
 });
+
+router.get('/login', function(req, res, next) {
+  res.render('login', {title:'Login'});
+});
+
+router.get('/user', function(req, res, next) {
+  res.render('user', {title:'User'});
+});
+
 module.exports = router;
