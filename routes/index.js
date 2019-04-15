2ffbabf51ed3b8d3f5b14f4c6929d732fd8ee898
Ana Antunes <anaantunes@cpejr.com.br>
2019-04-14 23:02:05 -0300

    Refinamento da página de cadastro (incompleto)
    Fiz um layout parecido com a página de login, falta colocar a foto de fundo

--- routes/index.js
+++ routes/index.js
@@ -44,22 +44,8 @@ router.post('/login',(req,res)=> {
 });
 
 router.post('/form',(req,res)=> {
-  const { username } = req.body.user;
-  const { persontype } = req.body.user;
-  const { fullname } = req.body.user;
-  const { register.type } = req.body.user;
-  const { type.type } = req.body.user;
-  const { adress.cep } = req.body.user;
-  const { address.street } = req.body.user;
-  const { adress.number } = req.body.user;
-  const { adress.complement } = req.body.user;
-  const { adress.city } = req.body.user;
-  const { adress.state } = req.body.user;
-  const { email.type } = req.body.user;
-  const { phone } = req.body.user;
-  const { cellphone } = req.body.user;
-  const { status.type } = req.body.user;
-)};
+  const { user } = req.body;
+});
 
 // GET /logout
 router.get('/logout', function(req, res, next) {
