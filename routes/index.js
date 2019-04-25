c1ee33c69ebbd00e919a89ff5fa87d2b7d040394
Ana Antunes <anaantunes@cpejr.com.br>
2019-04-25 11:09:21 -0300

    Mudança na pagina de form
    Coloquei o layout e o campo de senha no signup

--- routes/index.js
+++ routes/index.js
@@ -17,7 +17,7 @@ router.get('/login', (req, res) => {
 });
 
 router.get('/signup', (req, res) => {
-  res.render('form', {title:'signup'});
+  res.render('form', {title:'signup', layout:'layout'});
 });
 
 router.get('/user', function(req, res, next) {
