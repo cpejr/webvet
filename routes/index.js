7b22848c9a332f4213dd81db9ab0a3c86c4e562c
Marina Faria <maarina.faria@gmail.com>
2019-04-29 23:58:48 -0300

    Pequenos problemas de conflitos e erros gramaticais

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var firebase = require('firebase');
-const User = require('../models/user');
 var router = express.Router();
 const User = require('../models/user');
 const Kit = require('../models/kit');
