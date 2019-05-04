dadacdd518dd2ed382e116c9fcd11aab80873435
Bernardo <bernardoandrade@cpejr.com.br>
2019-05-04 08:55:47 -0300

    Metodos Put para alterar status dos usuarios
    Sao duas rotas .put para aceitar e recusar um user apartir de um botão. Falta linkar os usuarios e os botões à elas

--- routes/users.js
+++ routes/users.js
@@ -5,8 +5,26 @@ var mongoose = require('mongodb');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('users', { title: 'Usuários', layout: 'layoutDashboard.hbs' });
-  
+
 });
 
+router.put('/active', function(req, res, next) {
+  const user = {
+    status: 'Ativo'
+  };
+  User.update(req.params.id, user).then(() => {
+    res.redirect(`/users`)
+  })
+});
+
+
+router.put('/blocked', function(req, res, next) {
+  const user = {
+    status: 'Bloqueado'
+  };
+  User.update(req.params.id, user).then(() => {
+    res.redirect(`/users`)
+  })
+});
 
 module.exports = router;
