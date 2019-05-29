2c2d73b763503fa888201b27106c7604bbfe9fcc
Rafaela Moreira <moreiramrafaela@gmail.com>
2019-05-29 11:52:17 -0300

    conserto da tabela e função que reprova usuário

--- models/user.js
+++ models/user.js
@@ -127,7 +127,7 @@ class User {
 
   static delete(id) {
     return new Promise((resolve, reject) => {
-      UserModel.findByIdAndUpdate(id, { deleted: 1 }).then(() => {
+      UserModel.deleteOne({_id: id}).then(() => {
         resolve();
      }).catch((err) => {
        reject(err);
