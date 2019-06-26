e472f739713247d002f1566d97baceca4068c379
Ana Antunes <anaantunes@cpejr.com.br>
2019-06-26 10:40:54 -0300

    Edição nao funcionando
    O chaia me ajudou, entao ta quase, mas nao foi ainda

--- routes/profile.js
+++ routes/profile.js
@@ -10,8 +10,8 @@ const Email = require('../models/email');
 
 
 router.get('/show', auth.isAuthenticated, function(req, res, next) {
-  const user = req.session;
-  const id = req.session._id;
+  const user = req.body;
+  const id = req.params._id;
   User.getById(id).then((user) => {
     res.render('profile/show', { title: 'Perfil', layout: 'layoutDashboard.hbs', user, ...req.session });
 
@@ -25,27 +25,30 @@ router.get('/show', auth.isAuthenticated, function(req, res, next) {
 router.get('/edit/:_id', auth.isAuthenticated, function(req, res, next) {
   res.render('profile/edit', { title: 'Editar perfil', layout: 'layoutDashboard.hbs', ...req.session });
 });
-//
-// router.put('/edit/:_id', auth.isAuthenticated, function(req, res, next) {
-//   const id = req.session._id;
-//   const user = req.session;
-//     User.getById(id).then((user) => {
-//       const useredit = user;
-//       User.update(id, useredit).then(() => {
-//         res.render('profile/edit', { title: 'Editar perfil', layout: 'layoutDashboard.hbs', ...req.session });
-//         console.log("update");
-//         console.log(user);
-//         req.flash('success', 'Alterações no perfil realizadas');
-//       }).catch((error) => {
-//         console.log(error);
-//         res.redirect('/error');
-//       });
-//      }).catch((error) => {
-//        console.log(error);
-//        res.redirect('/error');
-//      });
-//   });
-//
+
+router.post('/edit/:_id', auth.isAuthenticated, function(req, res, next) {
+  const id = req.params._id;
+  console.log("Q");
+    User.getById(id).then((user) => {
+      const useredit = req.body;
+      console.log(user);
+      console.log("edit");
+      console.log(useredit);
+      User.update(id, user).then(() => {
+        console.log("update");
+        console.log(user);
+        res.render('profile/edit', { title: 'Editar perfil', layout: 'layoutDashboard.hbs', useredit, ...req.session });
+        req.flash('success', 'Alterações no perfil realizadas');
+      }).catch((error) => {
+        console.log(error);
+        res.redirect('/error');
+      });
+     }).catch((error) => {
+       console.log(error);
+       res.redirect('/error');
+     });
+  });
+
 
 
 module.exports = router;
