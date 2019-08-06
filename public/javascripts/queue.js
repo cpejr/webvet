7d306978b9e4c19ba4520a968aa197c9164bc232
Uiseman <uiseman@gmail.com>
2019-08-06 13:59:49 -0300

    Calibradores Finalizados

--- public/javascripts/queue.js
+++ public/javascripts/queue.js
@@ -916,45 +916,48 @@ const fumonisina = new jKanban({
   ],
   dropEl : function (el, target, source, sibling) {
     const samplenumber = el.dataset.eid;
-    var goTO=target;
-    console.log(goTO);
-    if(target =='_calibrator'){
-        var strId=el.dataset.eid; //id do card
-        if( el.dataset.eid=='P1'||el.dataset.eid=='P2'||el.dataset.eid=='P3'||el.dataset.eid=='P4'||el.dataset.eid=='P5') {//cards P não se movem
-               return false
-         }
-         else if( strId.indexOf("child")!=-1){ //basicamente todo elemento que contenha child no id
-           var id=el.dataset.eid;
-           scndFumonisina.removeElement(id);
-        } else {
-          return false // impede outros cards de entrarem no board dos calibradores
-        }
-    }
 
-    if( goTO.indexOf("workmap")!=-1) { //se o alvo for um board workmap qualquer
-        if( el.dataset.eid=='P1'||el.dataset.eid=='P2'||el.dataset.eid=='P3'||el.dataset.eid=='P4'||el.dataset.eid=='P5') {//cards originais
+    if (target == '_totest') {
+      $.post('/sample/totest/edit/fumonisina/' + samplenumber, () => {
 
-              var sonNumber=IdFumCount(); //essa função gera os id dos childs dos cards, para que estes naa tenham msm id
-              scndFumonisina.addElementStandart( goTO,
-               {  id: el.dataset.eid +'child'+ sonNumber.toString(),
-                  title: el.dataset.eid,
+      });
 
-               });
-
-           return false; // um card chil é criado no board alvo, mas o original retorna aos calibradores
-
-        } else {
-        //futuramente o metodo post para as amostras deve ficar aqui!
+      if (el.dataset.status == "Aguardando pagamento") {
+        el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Nova' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
+      } else {
+        el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'A corrigir' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
       }
+
+    }
+    if  (target == '_testing') {
+      $.post('/sample/testing/edit/fumonisina/' + samplenumber, () => {
+
+      });
+      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Em análise' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
+
+    }
+    if  (target == '_ownering') {
+      $.post('/sample/ownering/edit/fumonisina/' + samplenumber, () => {
+
+      });
+      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Aguardando pagamento' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
+
+    }
+    if  (target == '_waiting') {
+      $.post('/sample/waiting/edit/fumonisina/' + samplenumber, () => {
+
+      });
+      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Aguardando amostra' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
+
     }
 
-    if(target=='_scndTesting') {
-      if( el.dataset.eid=='P1'||el.dataset.eid=='P2'||el.dataset.eid=='P3'||el.dataset.eid=='P4'||el.dataset.eid=='P5') {//cards P não se movem para em analise
-             return false
-       }
+    if  (target == '_workmap') {
+      $.post('/sample/waiting/edit/fumonisina/' + samplenumber, () => {
+
+      });
+      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Mapa de trabalho' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
+
     }
-
-
   }
 });
 
@@ -1007,46 +1010,42 @@ var scndFumonisina = new jKanban({
   ],
   dropEl : function (el, target, source, sibling) {
     const samplenumber = el.dataset.eid;
+    var goTO=target;
+    console.log(goTO);
+    if(target =='_calibrator'){
+        var strId=el.dataset.eid; //id do card
+        if( el.dataset.eid=='P1'||el.dataset.eid=='P2'||el.dataset.eid=='P3'||el.dataset.eid=='P4'||el.dataset.eid=='P5') {//cards P não se movem
+               return false
+         }
+         else if( strId.indexOf("child")!=-1){ //basicamente todo elemento que contenha child no id
+           var id=el.dataset.eid;
+           scndFumonisina.removeElement(id);
+        } else {
+          return false // impede outros cards de entrarem no board dos calibradores
+        }
+    }
 
-    if (target == '_totest') {
-      $.post('/sample/totest/edit/aflatoxina/' + samplenumber, () => {
+    if( goTO.indexOf("workmap")!=-1) { //se o alvo for um board workmap qualquer
+        if( el.dataset.eid=='P1'||el.dataset.eid=='P2'||el.dataset.eid=='P3'||el.dataset.eid=='P4'||el.dataset.eid=='P5') {//cards originais
 
-      });
+              var sonNumber=IdFumCount(); //essa função gera os id dos childs dos cards, para que estes naa tenham msm id
+              scndFumonisina.addElementStandart( goTO,
+               {  id: el.dataset.eid +'child'+ sonNumber.toString(),
+                  title: el.dataset.eid,
 
-      if (el.dataset.status == "Aguardando pagamento") {
-        el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Nova' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
-      } else {
-        el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'A corrigir' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
+               });
+
+           return false; // um card chil é criado no board alvo, mas o original retorna aos calibradores
+
+        } else {
+        //futuramente o metodo post para as amostras deve ficar aqui!
       }
-
     }
-    if  (target == '_testing') {
-      $.post('/sample/testing/edit/aflatoxina/' + samplenumber, () => {
 
-      });
-      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Em análise' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
-
-    }
-    if  (target == '_ownering') {
-      $.post('/sample/ownering/edit/aflatoxina/' + samplenumber, () => {
-
-      });
-      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Aguardando pagamento' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
-
-    }
-    if  (target == '_waiting') {
-      $.post('/sample/waiting/edit/aflatoxina/' + samplenumber, () => {
-
-      });
-      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Aguardando amostra' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
-
-    }
-    if  (target == '_workmap') {
-      $.post('/sample/waiting/edit/aflatoxina/' + samplenumber, () => {
-
-      });
-      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Mapa de trabalho' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
-
+    if(target=='_scndTesting') {
+      if( el.dataset.eid=='P1'||el.dataset.eid=='P2'||el.dataset.eid=='P3'||el.dataset.eid=='P4'||el.dataset.eid=='P5') {//cards P não se movem para em analise
+             return false
+       }
     }
 
 
@@ -1199,6 +1198,27 @@ var scndZearalenona = new jKanban({
       id : '_calibrator',
       title  : 'Calibradores',
       class : 'success',
+      item: [
+          {
+            title:'P1',
+            id: 'P1'
+          },
+
+          {
+            title:'P2',
+            id: 'P2'
+          },
+
+          {
+            title:'P3',
+            id: 'P3'
+          },
+
+          {
+            title:'P4',
+            id: 'P4'
+          }
+      ]
     },
     {
       id : '_workmap1',
@@ -1209,51 +1229,56 @@ var scndZearalenona = new jKanban({
   ],
   dropEl : function (el, target, source, sibling) {
     const samplenumber = el.dataset.eid;
+    var goTO=target;
+    console.log(goTO);
+    if(target =='_calibrator'){
+        var strId=el.dataset.eid; //id do card
+        if( el.dataset.eid=='P1'||el.dataset.eid=='P2'||el.dataset.eid=='P3'||el.dataset.eid=='P4'||el.dataset.eid=='P5') {//cards P não se movem
+               return false
+         }
+         else if( strId.indexOf("child")!=-1){ //basicamente todo elemento que contenha child no id
+           var id=el.dataset.eid;
+           scndZearalenona.removeElement(id);
+        } else {
+          return false // impede outros cards de entrarem no board dos calibradores
+        }
+    }
 
-    if (target == '_totest') {
-      $.post('/sample/totest/edit/aflatoxina/' + samplenumber, () => {
+    if( goTO.indexOf("workmap")!=-1) { //se o alvo for um board workmap qualquer
+        if( el.dataset.eid=='P1'||el.dataset.eid=='P2'||el.dataset.eid=='P3'||el.dataset.eid=='P4'||el.dataset.eid=='P5') {//cards originais
 
-      });
+              var sonNumber=IdZCount(); //essa função gera os id dos childs dos cards, para que estes naa tenham msm id
+              scndZearalenona.addElementStandart( goTO,
+               {  id: el.dataset.eid +'child'+ sonNumber.toString(),
+                  title: el.dataset.eid,
 
-      if (el.dataset.status == "Aguardando pagamento") {
-        el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Nova' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
-      } else {
-        el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'A corrigir' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
+               });
+
+           return false; // um card chil é criado no board alvo, mas o original retorna aos calibradores
+
+        } else {
+        //futuramente o metodo post para as amostras deve ficar aqui!
       }
-
     }
-    if  (target == '_testing') {
-      $.post('/sample/testing/edit/aflatoxina/' + samplenumber, () => {
 
-      });
-      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Em análise' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
-
-    }
-    if  (target == '_ownering') {
-      $.post('/sample/ownering/edit/aflatoxina/' + samplenumber, () => {
-
-      });
-      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Aguardando pagamento' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
-
-    }
-    if  (target == '_waiting') {
-      $.post('/sample/waiting/edit/aflatoxina/' + samplenumber, () => {
-
-      });
-      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Aguardando amostra' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
-
-    }
-    if  (target == '_workmap') {
-      $.post('/sample/waiting/edit/aflatoxina/' + samplenumber, () => {
-
-      });
-      el.innerHTML = el.dataset.title + " "+ '<br><span  class="badge badge-secondary">' + 'Mapa de trabalho' + '</span>'+ " "+ '<span  class="badge badge-primary">' + el.dataset.analyst + '</span>';
-
+    if(target=='_scndTesting') {
+      if( el.dataset.eid=='P1'||el.dataset.eid=='P2'||el.dataset.eid=='P3'||el.dataset.eid=='P4'||el.dataset.eid=='P5') {//cards P não se movem para em analise
+             return false
+       }
     }
 
 
   }
 });
+//função de criação dos id dos Pchild para a fumonisina
+var countZ=0;
+
+function IdZCount ()
+{
+  console.log("count FUMOSININA is: " + countZ);
+     countZ++;
+    return countZ;
+}
 
 var zclicks = 1;
 function ZPlusButton() {
