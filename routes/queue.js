c1b34ceb1c8535cf98ebfcf813ef1aa962fee443
Fernando <fernandocouto@cpejr.com.br>
2019-10-07 17:15:32 -0300

    booleano do active ja esta dando bom

--- routes/queue.js
+++ routes/queue.js
@@ -15,4 +15,103 @@ router.get('/', auth.isAuthenticated, function(req, res, next) {
   res.render('admin/queue', { title: 'Queue', layout: 'layoutDashboard.hbs',...req.session});
 });
 
+router.post('/',function(req,res,next){
+  Sample.getAll().then((sample)=>{
+    const ativa = {
+        ocratoxina:{active:0,status:"xxx", mapReference:"sess"},
+    };
+    const afla_ativa = {
+      aflatoxina:{active: 0,status:"xxx",mapReference:"sess"},
+    };
+    const t2toxina_ativa = {
+      t2toxina:{active: 0 , status:"xxx", mapReference:"sess"}
+    };
+
+    const fumonisina_ativa = {
+      fumonisina:{active: 0, status:"xxx", mapReference:"sess"}
+    };
+
+    const zearalenona_ativa = {
+      zearalenona:{active:0 ,status:"xxx",mapReference:"sess"}
+    };
+
+    const deoxinivalenol_ativa = {
+      deoxinivalenol:{active: 0,status:"xxx",mapReference:"sess"}
+    };
+    
+    for (var i = 0; i < sample.length; i++) {
+      if(sample[i].ocratoxina.mapReference != 'Sem mapa'){
+        console.log(sample[i]._id);
+        ativa.ocratoxina.status = sample[i].ocratoxina.status;
+        ativa.ocratoxina.mapReference = sample[i].ocratoxina.mapReference;
+        Sample.update(sample[i]._id,ativa).then(()=>{
+          console.log(sample[i]._id);
+        }).catch((error)=>{
+         console.log(error);
+         });
+      }
+
+      if(sample[i].aflatoxina.mapReference != 'Sem mapa'){
+        console.log(sample[i]._id);
+        afla_ativa.aflatoxina.status = sample[i].aflatoxina.status;
+        afla_ativa.aflatoxina.mapReference = sample[i].aflatoxina.mapReference;
+        Sample.update(sample[i]._id,afla_ativa).then(()=>{
+          console.log(sample[i]._id);
+        }).catch((error)=>{
+         console.log(error);
+         });
+      }
+    
+      
+      if(sample[i].t2toxina.mapReference != 'Sem mapa'){
+        console.log(sample[i]._id);
+        t2toxina_ativa.t2toxina.status = sample[i].t2toxina.status;
+        t2toxina_ativa.t2toxina.mapReference = sample[i].t2toxina.mapReference;
+        Sample.update(sample[i]._id,t2toxina_ativa).then(()=>{
+          console.log(sample[i]._id);
+        }).catch((error)=>{
+         console.log(error);
+         });
+      }
+
+      if(sample[i].fumonisina.mapReference != 'Sem mapa'){
+        console.log(sample[i]._id);
+        fumonisina_ativa.fumonisina.status = sample[i].fumonisina.status;
+        fumonisina_ativa.fumonisina.mapReference = sample[i].fumonisina.mapReference;
+        Sample.update(sample[i]._id,fumonisina_ativa).then(()=>{
+          console.log(sample[i]._id);
+        }).catch((error)=>{
+         console.log(error);
+         });
+      }
+
+      
+      if(sample[i].zearalenona.mapReference != 'Sem mapa'){
+        console.log(sample[i]._id);
+        zearalenona_ativa.zearalenona.status = sample[i].zearalenona.status;
+        zearalenona_ativa.zearalenona.mapReference = sample[i].zearalenona.mapReference;
+        Sample.update(sample[i]._id,zearalenona_ativa).then(()=>{
+          console.log(sample[i]._id);
+        }).catch((error)=>{
+         console.log(error);
+         });
+      }
+     
+      if(sample[i].deoxinivalenol.mapReference != 'Sem mapa'){
+        console.log(sample[i]._id);
+        deoxinivalenol_ativa.deoxinivalenol.status = sample[i].deoxinivalenol.status;
+        deoxinivalenol_ativa.deoxinivalenol.mapReference = sample[i].deoxinivalenol.mapReference;
+        Sample.update(sample[i]._id,deoxinivalenol_ativa).then(()=>{
+          console.log(sample[i]._id);
+        }).catch((error)=>{
+         console.log(error);
+         });
+      }
+    }
+    res.render('admin/queue', { title: 'Queue', layout: 'layoutDashboard.hbs'});
+  }).catch((error)=>{
+    console.log(error);
+  });
+});
+
 module.exports = router;
