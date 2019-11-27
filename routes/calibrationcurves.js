516677afaa0810c3e1ab335cc5ecdaa4a48a8e64
Uiseman <uiseman@gmail.com>
2019-11-27 11:06:00 -0300

    estruturando deox

--- routes/calibrationcurves.js
+++ routes/calibrationcurves.js
@@ -13,6 +13,41 @@ function comparara(logb_bo_amostra,intercept,slope){
  }
  
  router.get('/', auth.isAuthenticated,async function(req, res, next) {
+   //função rodando em paralelo
+   var deox_p = new Array;
+   var deox_array = new Array;
+   Kit.getActiveDeox().then((deoxArray)=>{ 
+     let deoxKit= deoxArray[0];
+     deox_p[0] = deoxKit.calibrators.P1;
+     deox_p[1] = deoxKit.calibrators.P2;
+     deox_p[2] = deoxKit.calibrators.P3;
+     deox_p[3] = deoxKit.calibrators.P4;
+     deox_p[4] = deoxKit.calibrators.P5;
+
+     var b_b0 = new Array;
+     var ln_b_b0 = new Array; // eixo y
+     cont_b0 = 0;
+     var log_concentracao = [Math.log10(4),Math.log10(10),Math.log10(20),Math.log10(40)]; //eixo x
+     for (var i = 0; i < 4; i++){
+       b_b0[cont_b0] = aflas_p[i+1].absorbance/aflas_p[0].absorbance;
+       cont_b0++;
+     }
+
+     for (var i = 0; i < b_b0.length; i++) {
+      ln_b_b0[i] = Math.log10(b_b0[i]/(1-b_b0[i]));
+    }
+    const result = regression.linear([[log_concentracao[0],ln_b_b0[0]],[log_concentracao[1],ln_b_b0[1]],[log_concentracao[2],ln_b_b0[2]]]);
+    const slope = result.equation[0];// slope
+    const yIntercept = result.equation[1];// intercept
+    const result2 = regression.linear([[ln_b_b0[0],log_concentracao[0]],[ln_b_b0[1],log_concentracao[1]],[ln_b_b0[2],log_concentracao[2]]]);
+    const gradient2 = result2.equation[0];
+    const yIntercp2 = result2.equation[1];
+    const fifty_inhibition = Math.pow(10,yIntercp2); //50% inhibition
+
+   }).catch((error)=>{
+    console.log(error);
+  });
+
   //teste kit afla
   var aflas_p = new Array;
   var sample_array = new Array;
