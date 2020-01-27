6f731b9e030c6e263ae7cd1cdbc32f795180858f
Arthur Braga <Arthurmbraga1@gmail.com>
2020-01-27 14:19:08 -0200

    Campo resultado agora exibe a média

--- routes/sampleresult.js
+++ routes/sampleresult.js
@@ -92,12 +92,13 @@ router.get('/', async function (req, res, next) {
     console.log("absss");
 
     console.log(amostras_afla[0].aflatoxina.absorbance);
-    //console.log(amostras_afla[1].aflatoxina.absorbance);
-
 
     var Afla_log_b_b0 = new Array;
+    var Afla_log_b_b0_2 = new Array;
+
     for (let i = 0; i < amostras_afla.length; i++) {
       Afla_log_b_b0[i] = Math.log10((amostras_afla[i].aflatoxina.absorbance / Aflaabsorbance_p[0]) / (1 - (amostras_afla[i].aflatoxina.absorbance / Aflaabsorbance_p[0])));
+      Afla_log_b_b0_2[i] = Math.log10((amostras_afla[i].aflatoxina.absorbance2 / Aflaabsorbance_p[0]) / (1 - (amostras_afla[i].aflatoxina.absorbance2 / Aflaabsorbance_p[0])));
       console.log("log b bo");
       console.log(Afla_log_b_b0[i]);
 
@@ -106,10 +107,10 @@ router.get('/', async function (req, res, next) {
 
 
     for (let i = 0; i < Afla_log_b_b0.length; i++) {
-      console.log("atroei- ")
       var avg = (amostras_afla[i].aflatoxina.absorbance + amostras_afla[i].aflatoxina.absorbance2) / 2;
+      var avgcompara = (comparara(Afla_log_b_b0[i], yIntercept, slope) + comparara(Afla_log_b_b0_2[i], yIntercept, slope)) / 2;
       resultado_afla[i] = {
-        compara: comparara(Afla_log_b_b0[i], yIntercept, slope),
+        compara: avgcompara,
         average: avg,
         number: amostras_afla[i].samplenumber,
         changed_workmap: i != 0 && amostras_afla[i - 1].aflatoxina.mapReference != amostras_afla[i].aflatoxina.mapReference
@@ -185,16 +186,18 @@ router.get('/', async function (req, res, next) {
     console.log(yIntercept_deox);
 
     var Deox_log_b_b0 = new Array;
-    //log b/b0 = Math.log10((absorvanciaAmostras/AbsorvanciaP1)/(1-(absorvanciaAmostras/AbsorvanciaP1)))
+    var Deox_log_b_b0_2 = new Array;
+
     for (let i = 0; i < amostras_deox.length; i++) {
       Deox_log_b_b0[i] = Math.log10((amostras_deox[i].deoxinivalenol.absorbance / DeoxAbsorbance_p[0]) / (1 - (amostras_deox[i].deoxinivalenol.absorbance / DeoxAbsorbance_p[0])));
-
+      Deox_log_b_b0_2[i] = Math.log10((amostras_deox[i].deoxinivalenol.absorbance2 / DeoxAbsorbance_p[0]) / (1 - (amostras_deox[i].deoxinivalenol.absorbance2 / DeoxAbsorbance_p[0])));
     }
 
     for (let i = 0; i < Deox_log_b_b0.length; i++) {
       var avg = (amostras_deox[i].deoxinivalenol.absorbance + amostras_deox[i].deoxinivalenol.absorbance2) / 2;
+      var avgcompara = (comparara(Deox_log_b_b0[i], yIntercept_deox, slope_deox) + comparara(Deox_log_b_b0_2[i], yIntercept_deox, slope_deox)) / 2;
       resultado_deox[i] = {
-        compara: comparara(Deox_log_b_b0[i], yIntercept_deox, slope_deox),
+        compara: avgcompara,
         average: avg,
         number: amostras_deox[i].samplenumber,
         changed_workmap: i != 0 && amostras_deox[i - 1].deoxinivalenol.mapReference != amostras_deox[i].deoxinivalenol.mapReference
@@ -281,17 +284,20 @@ router.get('/', async function (req, res, next) {
     console.log(yIntercept_ota);
 
     var Ota_log_b_b0 = new Array;
-    //log b/b0 = Math.log10((absorvanciaAmostras/AbsorvanciaP1)/(1-(absorvanciaAmostras/AbsorvanciaP1)))
+    var Ota_log_b_b0_2 = new Array;
+
     for (let i = 0; i < amostras_ota.length; i++) {
       Ota_log_b_b0[i] = Math.log10((amostras_ota[i].ocratoxina.absorbance / OtaAbsorbance_p[0]) / (1 - (amostras_ota[i].ocratoxina.absorbance / OtaAbsorbance_p[0])));
-
+      Ota_log_b_b0_2[i] = Math.log10((amostras_ota[i].ocratoxina.absorbance2 / OtaAbsorbance_p[0]) / (1 - (amostras_ota[i].ocratoxina.absorbance2 / OtaAbsorbance_p[0])));
     }
+
     console.log(Ota_log_b_b0);
 
     for (let i = 0; i < Ota_log_b_b0.length; i++) {
       var avg = (amostras_ota[i].ocratoxina.absorbance + amostras_ota[i].ocratoxina.absorbance2) / 2;
+      var avgcompara = (comparara(Ota_log_b_b0[i], yIntercept_ota, slope_ota) + comparara(Ota_log_b_b0_2[i], yIntercept_ota, slope_ota)) / 2;
       resultado_ota[i] = {
-        compara: comparara(Ota_log_b_b0[i], yIntercept_ota, slope_ota),
+        compara: avgcompara,
         average: avg,
         number: amostras_ota[i].samplenumber,
         changed_workmap: i != 0 && amostras_ota[i - 1].ocratoxina.mapReference != amostras_ota[i].ocratoxina.mapReference
@@ -364,16 +370,19 @@ router.get('/', async function (req, res, next) {
 
 
     var T2_log_b_b0 = new Array;
-    //log b/b0 = Math.log10((absorvanciaAmostras/AbsorvanciaP1)/(1-(absorvanciaAmostras/AbsorvanciaP1)))
+    var T2_log_b_b0_2 = new Array;
+
     for (let i = 0; i < amostras_t2.length; i++) {
       T2_log_b_b0[i] = Math.log10((amostras_t2[i].t2toxina.absorbance / T2absorbance_p[0]) / (1 - (amostras_t2[i].t2toxina.absorbance / T2absorbance_p[0])));
+      T2_log_b_b0_2[i] = Math.log10((amostras_t2[i].t2toxina.absorbance2 / T2absorbance_p[0]) / (1 - (amostras_t2[i].t2toxina.absorbance2 / T2absorbance_p[0])));
     }
     console.log(T2_log_b_b0);
 
     for (let i = 0; i < T2_log_b_b0.length; i++) {
       var avg = (amostras_t2[i].t2toxina.absorbance + amostras_t2[i].t2toxina.absorbance2) / 2;
+      var avgcompara = (comparara(T2_log_b_b0[i], yIntercept_t2, slope_t2) + comparara(T2_log_b_b0_2[i], yIntercept_t2, slope_t2)) / 2;
       resultado_t2[i] = {
-        compara: comparara(T2_log_b_b0[i], yIntercept_t2, slope_t2),
+        compara: avgcompara,
         average: avg,
         number: amostras_t2[i].samplenumber,
         changed_workmap: i != 0 && amostras_t2[i - 1].t2toxina.mapReference != amostras_t2[i].t2toxina.mapReference
@@ -450,16 +459,20 @@ router.get('/', async function (req, res, next) {
     console.log(yIntercept_zea);
 
     var Zea_log_b_b0 = new Array;
-    //log b/b0 = Math.log10((absorvanciaAmostras/AbsorvanciaP1)/(1-(absorvanciaAmostras/AbsorvanciaP1)))
+    var Zea_log_b_b0_2 = new Array;
+
     for (let i = 0; i < amostras_zea.length; i++) {
       Zea_log_b_b0[i] = Math.log10((amostras_zea[i].zearalenona.absorbance / ZeaAbsorbance_p[0]) / (1 - (amostras_zea[i].zearalenona.absorbance / ZeaAbsorbance_p[0])));
+      Zea_log_b_b0_2[i] = Math.log10((amostras_zea[i].zearalenona.absorbance2 / ZeaAbsorbance_p[0]) / (1 - (amostras_zea[i].zearalenona.absorbance2 / ZeaAbsorbance_p[0])));
     }
 
     console.log(Zea_log_b_b0);
     for (let i = 0; i < Zea_log_b_b0.length; i++) {
       var avg = (amostras_zea[i].zearalenona.absorbance + amostras_zea[i].zearalenona.absorbance2) / 2;
+      var avgcompara = (comparara(Zea_log_b_b0[i], yIntercept_zea, slope_zea) + comparara(Zea_log_b_b0_2[i], yIntercept_zea, slope_zea)) / 2;
+
       resultado_zea[i] = {
-        compara: comparara(Zea_log_b_b0[i], yIntercept_zea, slope_zea),
+        compara: avgcompara,
         average: avg,
         number: amostras_zea.samplesnumber,
         changed_workmap: i != 0 && amostras_zea[i - 1].zearalenona.mapReference != amostras_zea[i].zearalenona.mapReference
@@ -530,18 +543,20 @@ router.get('/', async function (req, res, next) {
     console.log(yIntercept_fbs);
 
     var Fbs_log_b_b0 = new Array;
-    //log b/b0 = Math.log10((absorvanciaAmostras/AbsorvanciaP1)/(1-(absorvanciaAmostras/AbsorvanciaP1)))
+    var Fbs_log_b_b0_2 = new Array;
+
     for (let i = 0; i < amostras_fbs.length; i++) {
       Fbs_log_b_b0[i] = Math.log10((amostras_fbs[i].fumonisina.absorbance / Fbsabsorbance_p[0]) / (1 - (amostras_fbs[i].fumonisina.absorbance / Fbsabsorbance_p[0])));
-
+      Fbs_log_b_b0_2[i] = Math.log10((amostras_fbs[i].fumonisina.absorbance2 / Fbsabsorbance_p[0]) / (1 - (amostras_fbs[i].fumonisina.absorbance2 / Fbsabsorbance_p[0])));
     }
     console.log(Fbs_log_b_b0);
 
 
     for (let i = 0; i < Fbs_log_b_b0.length; i++) {
       var avg = (amostras_fbs[i].fumonisina.absorbance + amostras_fbs[i].fumonisina.absorbance2) / 2;
+      var avgcompara = (comparara(Fbs_log_b_b0[i], yIntercept_fbs, slope_fbs) + comparara(Fbs_log_b_b0_2[i], yIntercept_fbs, slope_fbs)) / 2;
       resultado_fbs[i] = {
-        compara: vcomparara(Fbs_log_b_b0[i], yIntercept_fbs, slope_fbs),
+        compara: avgcompara,
         average: avg,
         number: amostras_fbs[i].samplenumber,
         changed_workmap: i != 0 && amostras_fbs[i - 1].fumonisina.mapReference != amostras_fbs[i].fumonisina.mapReference
