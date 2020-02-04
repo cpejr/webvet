a9943f8416dcf96f7161fca415fb6463f4f4349a
Arthur Braga <Arthurmbraga1@gmail.com>
2020-02-04 18:01:25 -0200

    Correção de require

--- models/sample.js
+++ models/sample.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const mongoose = require('mongoose');
-const Counter = require('counter');
+const Counter = require('../models/counter');
 var data = new Date();
 var yyyy = data.getFullYear();
 
