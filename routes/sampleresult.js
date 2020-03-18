0a3a95f1ec6b4f04d0cc195e64e66df17a2fbbc7
João Prates <jvdprates@hotmail.com>
2020-03-18 19:35:07 -0300

    Reescrevendo função calcular da rota sampleresult

--- routes/sampleresult.js
+++ routes/sampleresult.js
@@ -15,87 +15,90 @@ function comparara(logb_bo_amostra, intercept, slope) {
 router.get('/', async function (req, res, next) {
 
 
-  function calcular(toxinafull, toxinasigla) {
+  function calcular(nomeToxina, siglaToxina) {
     return new Promise(async (resolve, reject) => {
+      Kit.getActive(siglaToxina).then((kit) => {
+        if (kit) {
+          Workmap.getByIdArray(kit.mapArray).then((maps) => {
+            for (let j = 0; j < maps.length; j++) {
+              let actualMap = maps[j];
+              for (let k = 0; k < actualMap.length; k++) {
+                samples_id.push(actualMap.sampleArray[k]);
+              }
+            }
+            Sample.getActiveByIdArray(samples_id, nomeToxina).then((samples) => {
+              let allcalibrators = kit.calibrators;
+              let p_concentration = [];
+              let p_absorvance = [];
+              let resultado = [];
+              let samples_id = []
 
+              for (let i = 0; i < allcalibrators.length; i++) {
+                let curCal = "P" + i;
+                let actualCalib = allcalibrators.curCal
+                p_concentration.push(actualCalib.concentration);
+                p_absorvance.push(actualCalib.absorbance);
+              }
 
-      let kit = await Kit.getActive(toxinasigla);
-      if (kit !== null) {
-        var mapas = [];
-        var amostras = [];
-        var p_concentration = [];
-        var p_absorvance = [];
-        var resultado = [];
+              let log_concentracao = [Math.log10(p_concentration[1]), Math.log10(p_concentration[2]), Math.log10(p_concentration[3]), Math.log10(p_concentration[4])]; //eixo x
+              let b_b0 = [];
+              let ln_b_b0 = [];
 
-        //Parte responsável por pegar a concentracao e absorvancia settadas no kit ativo
-        p_concentration[0] = kit.calibrators.P1.concentration;
-        p_concentration[1] = kit.calibrators.P2.concentration;
-        p_concentration[2] = kit.calibrators.P3.concentration;
-        p_concentration[3] = kit.calibrators.P4.concentration;
-        p_concentration[4] = kit.calibrators.P5.concentration;
+              for (let m = 0; m < 4; m++) {
+                b_b0.push(p_absorvance[m + 1] / p_absorvance[0]);
+              }
 
-        p_absorvance[0] = kit.calibrators.P1.absorbance;
-        p_absorvance[1] = kit.calibrators.P2.absorbance;
-        p_absorvance[2] = kit.calibrators.P3.absorbance;
-        p_absorvance[3] = kit.calibrators.P4.absorbance;
-        p_absorvance[4] = kit.calibrators.P5.absorbance;
+              for (let n = 0; n < b_b0.length; n++) {
+                ln_b_b0[n] = Math.log10(b_b0[n] / (1 - b_b0[n]));
+              }
 
-        //parte responsável por pegar as amostras do kit, logo  através do kit ativo de afla pega na variável mapArray o id dos mapas que estão sendo utilizados naqueles kits
-        mapas = await Workmap.getByIdArray(kit.mapArray);
+              const result = regression.linear([[log_concentracao[0], ln_b_b0[0]], [log_concentracao[1], ln_b_b0[1]], [log_concentracao[2], ln_b_b0[2]]]);
+              const slope = result.equation[0];// slope
+              const yIntercept = result.equation[1];// intercept
 
-        var samples_id = [];
-        //Após ter os ids dos mapas de trabalho que estão sendo utilizados roda um for para percorrer todos os mapas e um for dentro desse para acessar todas as amostras em cada mapa
-        for (let j = 0; j < mapas.length; j++)
-          for (let i = 0; i < mapas[j].samplesArray.length; i++)
-            samples_id.push(mapas[j].samplesArray[i]);
+              let log_b_b0 = [];
+              let log_b_b0_2 = [];
 
-        var log_concentracao = [Math.log10(p_concentration[1]), Math.log10(p_concentration[2]), Math.log10(p_concentration[3]), Math.log10(p_concentration[4])]; //eixo x
-        var b_b0 = [];
-        var ln_b_b0 = [];
+              for (let h = 0; h < samples.length; h++) {
+                log_b_b0[h] = Math.log10((samples[h][nomeToxina].absorbance / p_absorvance[0]) / (1 - (samples[h][nomeToxina].absorbance / p_absorvance[0])));
+                log_b_b0_2[h] = Math.log10((samples[h][nomeToxina].absorbance2 / p_absorvance[0]) / (1 - (samples[h][nomeToxina].absorbance2 / p_absorvance[0])));
+              }
+              
+              for (let i = 0; i < log_b_b0.length; i++) {
+                var avg = (samples[i][nomeToxina].absorbance + samples[i][nomeToxina].absorbance2) / 2;
+                var avgcompara = (comparara(log_b_b0[i], yIntercept, slope) + comparara(log_b_b0_2[i], yIntercept, slope)) / 2;
 
-        for (var i = 0; i < 4; i++)
-          b_b0[i] = p_absorvance[i + 1] / p_absorvance[0];
+                resultado.push({
+                  compara: avgcompara,
+                  average: avg,
+                  number: samples[i].samplenumber,
+                  changed_workmap: i != 0 && samples[i - 1][nomeToxina].workmapId != samples[i][nomeToxina].workmapId,
+                  _id: samples[i]._id
+                });
+      
+                if (isNaN(resultado[i].compara)) {
+                  resultado[i].compara = null;
+                };
 
+                Sample.updateResult(resultado[i]._id, nomeToxina, resultado[i].compara);
+              };
 
-        for (var i = 0; i < b_b0.length; i++)
-          ln_b_b0[i] = Math.log10(b_b0[i] / (1 - b_b0[i]));
+              resolve(resultado);
 
-        const result = regression.linear([[log_concentracao[0], ln_b_b0[0]], [log_concentracao[1], ln_b_b0[1]], [log_concentracao[2], ln_b_b0[2]]]);
-        const slope = result.equation[0];// slope
-        const yIntercept = result.equation[1];// intercept
+            }).catch((err) => {
+              reject(err);
+            })
+          }).catch((err) => {
+            reject(err);
+          });
 
-        var log_b_b0 = new Array;
-        var log_b_b0_2 = new Array;
+        } else {
+          resolve(null);
+        };
 
-        amostras = await Sample.getActiveByIdArray(samples_id, toxinafull);
-
-        for (let i = 0; i < amostras.length; i++) {
-          log_b_b0[i] = Math.log10((amostras[i][toxinafull].absorbance / p_absorvance[0]) / (1 - (amostras[i][toxinafull].absorbance / p_absorvance[0])));
-          log_b_b0_2[i] = Math.log10((amostras[i][toxinafull].absorbance2 / p_absorvance[0]) / (1 - (amostras[i][toxinafull].absorbance2 / p_absorvance[0])));
-        }
-
-        for (let i = 0; i < log_b_b0.length; i++) {
-          var avg = (amostras[i][toxinafull].absorbance + amostras[i][toxinafull].absorbance2) / 2;
-          var avgcompara = (comparara(log_b_b0[i], yIntercept, slope) + comparara(log_b_b0_2[i], yIntercept, slope)) / 2;
-
-          resultado[i] = {
-            compara: avgcompara,
-            average: avg,
-            number: amostras[i].samplenumber,
-            changed_workmap: i != 0 && amostras[i - 1][toxinafull].workmapId != amostras[i][toxinafull].workmapId,
-            _id: amostras[i]._id
-          };
-
-          if (isNaN(resultado[i].compara)) {
-            resultado[i].compara = null;
-          }
-          Sample.updateResult(resultado[i]._id, toxinafull, resultado[i].compara);
-        }
-
-        resolve(resultado);
-      }
-      else
-        resolve(null);
+      }).catch((err) => {
+        reject(err);
+      });
     });
   }
 
