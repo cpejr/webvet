c511c96614945ac85613e48bd4698ddc7048b144
Arthur Braga <Arthurmbraga1@gmail.com>
2020-06-02 03:00:48 -0300

    Alterações para heroku

--- app.js
+++ app.js
@@ -81,7 +81,18 @@ const config = {
 };
 firebase.initializeApp(config);
 
-const serviceAccont = require('./serviceAccountKey.json');
+const serviceAccont = {
+  type: process.env.ADMIN_TYPE,
+  project_id: process.env.ADMIN_PROJECT_ID,
+  private_key_id: process.env.ADMIN_PRIVATE_KEY_ID,
+  private_key: process.env.ADMIN_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.ADMIN_CLIENT_EMAIL,
+  client_id: process.env.ADMIN_CLIENT_ID,
+  auth_uri: process.env.ADMIN_AUTH_URI,
+  token_uri: process.env.ADMIN_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.ADMIN_AUTH_PROVIDER_X509_CERT_URL,
+  client_x509_cert_url: process.env.ADMIN_CLIENT_X509_CERT_URL,
+}
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccont),
   databaseURL: process.env.FIREBASE_DATABASE_URL
