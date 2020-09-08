27d4db6fe4d8c9ebad60cec66ad001cdd69af097
Giovanna <giovanna.pimenta11@gmail.com>
2020-09-08 16:21:47 -0300

    pequenas alterações

--- app.js
+++ app.js
@@ -277,9 +277,15 @@ app.use((err, req, res, next) => {
   res.locals.message = err.message;
   res.locals.error = req.app.get("env") === "development" ? err : {};
 
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
+  if (err.status !== 404) {
+    // render the error page
+    res.status(err.status || 500);
+    res.render("error");
+  }else{
+    res.status(err.status || 500);
+    res.render("notFound");
+  }
+
 });
 
 module.exports = app;
