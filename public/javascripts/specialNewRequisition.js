37bf0a32f52d39056ebe2088b4e109d3dcee2f77
João Prates <jvdprates@hotmail.com>
2020-11-24 16:43:16 -0300

    Fix: Melhorando legibilidade do código

--- public/javascripts/specialNewRequisition.js
+++ public/javascripts/specialNewRequisition.js
@@ -1,24 +1,46 @@
 let users = JSON.parse($("#users").val());
 
+function findAndUpdateValue(elementId, newData) {
+  //Acha o elemento com o ID passado e muda o seu valor para o dado passado.
+  //Se for undefined vira "" por padrão.
+  $(`${elementId}`).val(newData !== undefined ? newData : "");
+}
+function findAndSelectCorrectly(elementId, newData, defaultId) {
+  //Seleciona a opção com o valor passado do select com o id passado.
+  //Se o valor é undefined seleciona pelo id default.
+  if (newData !== undefined) {
+    $(`${elementId} option[value="${newData}"]`).prop("selected", true);
+  } else {
+    $(`${defaultId}`).prop("selected", true);
+  }
+}
+
 // Logica para setar os dados do endereço ao selecionar o usuário no select
 $("#adminUser").on("change", function (event) {
-    let selectedUser = users.find(user => user._id === event.target.value);
-    $("#fullname").attr("value", selectedUser.fullname !== undefined ? selectedUser.fullname : "");
-    $("#register").val(selectedUser.register !== undefined ? selectedUser.register : "");
-    $("#IE").val(selectedUser.address.ie !== undefined ? selectedUser.IE : "");
-    $("#street").val(selectedUser.address.street !== undefined ? selectedUser.address.street : "");
-    $("#number").val(selectedUser.address.number !== undefined ? selectedUser.address.number : "");
-    $("#complement").val(selectedUser.address.complement !== undefined ? selectedUser.address.complement : "");
-    $("#neighborhood").val(selectedUser.address.neighborhood !== undefined ? selectedUser.address.neighborhood : "")
-    $("#city").val(selectedUser.address.city !== undefined ? selectedUser.address.city : "");
-    $("#cep").val(selectedUser.address.cep !== undefined ? selectedUser.address.cep : "");
-    if(selectedUser.address.state !== undefined){
-        $(`#state option[value="${selectedUser.address.state}"]`).prop("selected",true);
-    } else {
-        $("#defaultStateOption").prop("selected",true);
-    }
-    $("#responsible").val(selectedUser.fullname !== undefined ? selectedUser.fullname : "");
-    $("#email").val(selectedUser.email !== undefined ? selectedUser.email : "");
-    $("#phone").val(selectedUser.phone !== undefined ? selectedUser.phone : "");
-    $("#cellphone").val(selectedUser.cellphone !== undefined ? selectedUser.cellphone : "");
+  //Dados de Conbrança
+  let selectedUser = users.find((user) => user._id === event.target.value);
+  findAndUpdateValue("#fullname", selectedUser.fullname);
+  findAndUpdateValue("#register", selectedUser.register);
+  findAndUpdateValue("#IE", selectedUser.address.ie);
+  findAndUpdateValue("#street", selectedUser.address.street);
+  findAndUpdateValue("#number", selectedUser.address.number);
+  findAndUpdateValue("#complement", selectedUser.address.complement);
+  findAndUpdateValue("#neighborhood", selectedUser.address.neighborhood);
+  findAndUpdateValue("#city", selectedUser.address.city);
+  findAndUpdateValue("#cep", selectedUser.address.cep);
+  findAndSelectCorrectly(
+    "#state",
+    selectedUser.address.state,
+    "#defaultStateOption"
+  );
+
+  //Dados de Contato
+  findAndUpdateValue("#responsible", selectedUser.fullname);
+  findAndUpdateValue("#email", selectedUser.email);
+  findAndUpdateValue("#phone", selectedUser.phone);
+  findAndUpdateValue("#cellphone", selectedUser.cellphone);
+
+  //Requisição de Análise
+  findAndUpdateValue("#reqCity", selectedUser.address.city);
+  findAndSelectCorrectly("#reqState", selectedUser.address.state, "#defaultStateReqOption");
 });
