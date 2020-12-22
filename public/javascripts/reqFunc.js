9c0ac8e35e9765a4e16cb44e6032ff3679addf20
Arthur Martins Braga <arthurmbraga1@gmail.com>
2020-12-22 18:59:28 -0300

    fix: autoimport user data

--- public/javascripts/reqFunc.js
+++ public/javascripts/reqFunc.js
@@ -1,11 +1,9 @@
-
 var num = 2;
 function addInput() {
-    if (num < 11) {
-        var newInput = document.createElement("div");
+  if (num < 11) {
+    var newInput = document.createElement("div");
 
-        let html =
-            `<div class="requisition-text col-md-8">
+    let html = `<div class="requisition-text col-md-8">
                 <input required type="text" name="requisition[sampleVector][${num}][name]" id="sample${num}" class="requisition-text floating-label-field"
                 placeholder="Amostra ${num}">
                 <label for='sample${num}' class="floating-label">Amostra ${num}</label>
@@ -17,139 +15,108 @@ function addInput() {
                 </div>
             </div>`;
 
-        newInput.innerHTML = html;
-        newInput.className = "form-row";
-        document.getElementById('samples').appendChild(newInput);
-        num++;
-    } else {
-        req.flash('alert', 'Número máximo de requisições atingido');
-    }
+    newInput.innerHTML = html;
+    newInput.className = "form-row";
+    document.getElementById("samples").appendChild(newInput);
+    num++;
+  } else {
+    req.flash("alert", "Número máximo de requisições atingido");
+  }
 }
-var checados = document.getElementsByName('polpa');
-var inputs = document.getElementsByName('requisition[sampleVector]');
-var polpaCitrica = " com polpa cítrica"
+var checados = document.getElementsByName("polpa");
+var inputs = document.getElementsByName("requisition[sampleVector]");
+var polpaCitrica = " com polpa cítrica";
 
 for (var i = 0; i < checados.lenght; i++) {
-    if (checados[i].checked) {
-        inputs[i] = input[i].concat(polpaCitrica)
-    }
+  if (checados[i].checked) {
+    inputs[i] = input[i].concat(polpaCitrica);
+  }
 }
 
-//fução preencher automático
-var fullname = document.getElementById("fullname").value;
-var cpf = document.getElementById("cpf").value;
-var ie = document.getElementById("I.E").value;
-var street = document.getElementById("street").value;
-var number = document.getElementById("number").value;
-var complement = document.getElementById("complement").value;
-var neighborhood = document.getElementById("neighborhood").value;
-var city = document.getElementById("city").value;
-var state = document.getElementById("state").value;
-var cep = document.getElementById("cep").value;
-var phone = document.getElementById("phone").value;
-var cellphone = document.getElementById("cellphone").value;
-var email = document.getElementById("email").value;
-
-
-var firstClick = 1;
-function clearData(){
-
-    if (firstClick) {
-        $('#fullname').each(function () {
-            $(this).val('');
-        });
-        $('#cpf').each(function () {
-            $(this).val('');
-        });
-        $('#I.E').each(function () {
-            $(this).val('');
-        });
-        $('#street').each(function () {
-            $(this).val('');
-        });
-        $('#number').each(function () {
-            $(this).val('');
-        });
-        $('#complement').each(function () {
-            $(this).val('');
-        });
-        $('#neighborhood').each(function () {
-            $(this).val('');
-        });
-        $('#city').each(function () {
-            $(this).val('');
-        });
-        $('#state').each(function () {
-            $(this).val('');
-        });
-        $('#cep').each(function () {
-            $(this).val('');
-        });
-        $('#phone').each(function () {
-            $(this).val('');
-        });
-        $('#cellphone').each(function () {
-            $(this).val('');
-        });
-        $('#email').each(function () {
-            $(this).val('');
-        });
-        firstClick = !firstClick;
-    }
-    else {
-        $('#fullname').each(function () {
-            $(this).val(fullname);
-        });
-        $('#cpf').each(function () {
-            $(this).val(cpf);
-        });
-        $('#I.E').each(function () {
-            $(this).val(ie);
-        });
-        $('#street').each(function () {
-            $(this).val(street);
-        });
-        $('#number').each(function () {
-            $(this).val(number);
-        });
-        $('#complement').each(function () {
-            $(this).val(complement);
-        });
-        $('#neighborhood').each(function () {
-            $(this).val(neighborhood);
-        });
-        $('#city').each(function () {
-            $(this).val(city);
-        });
-        $('#state').each(function () {
-            $(this).val(state);
-        });
-        $('#cep').each(function () {
-            $(this).val(cep);
-        });
-        $('#phone').each(function () {
-            $(this).val(phone);
-        });
-        $('#cellphone').each(function () {
-            $(this).val(cellphone);
-        });
-        $('#email').each(function () {
-            $(this).val(email);
-        });
-        firstClick = !firstClick;
-
-    }
-
-};
-
-$('.destination').change(function () {
-    if ($('#Others').is(':checked')) {
-        $('#Other-destination').removeClass('form-disabled');
-    } else {
-        $('#Other-destination').addClass('form-disabled');
-    }
+$(".destination").change(function () {
+  if ($("#Others").is(":checked")) {
+    $("#Other-destination").removeClass("form-disabled");
+  } else {
+    $("#Other-destination").addClass("form-disabled");
+  }
 });
 
 $("#Other-destination").keyup(function () {
-    $('#Others').val($(this).find('input').val());
+  $("#Others").val($(this).find("input").val());
+});
+
+function findAndUpdateValue(elementId, newData) {
+  //Acha o elemento com o ID passado e muda o seu valor para o dado passado.
+  //Se for undefined vira "" por padrão.
+  $(`${elementId}`).val(newData !== undefined ? newData : "");
+}
+function findAndSelectCorrectly(elementId, newData, defaultId) {
+  //Seleciona a opção com o valor passado do select com o id passado.
+  //Se o valor é undefined seleciona pelo id default.
+  if (newData !== undefined) {
+    $(`${elementId} option[value="${newData}"]`).prop("selected", true);
+  } else {
+    $(`${defaultId}`).prop("selected", true);
+  }
+}
+
+// Logica para setar os dados do endereço ao selecionar o usuário no select
+$("#adminUser").on("change", async function (event) {
+  console.log("aa");
+  //Dados de Conbrança  
+  const target = event.target.value;
+  const userData = await $.get(`/users/byid/${target}`)
+
+  findAndUpdateValue("#fullname", userData.fullname);
+  findAndUpdateValue("#register", userData.register);
+  findAndUpdateValue(
+    "#IE",
+    userData.address ? userData.address.ie : undefined
+  );
+  findAndUpdateValue(
+    "#street",
+    userData.address ? userData.address.street : undefined
+  );
+  findAndUpdateValue(
+    "#number",
+    userData.address ? userData.address.number : undefined
+  );
+  findAndUpdateValue(
+    "#complement",
+    userData.address ? userData.address.complement : undefined
+  );
+  findAndUpdateValue(
+    "#neighborhood",
+    userData.address ? userData.address.neighborhood : undefined
+  );
+  findAndUpdateValue(
+    "#city",
+    userData.address ? userData.address.city : undefined
+  );
+  findAndUpdateValue(
+    "#cep",
+    userData.address ? userData.address.cep : undefined
+  );
+  findAndSelectCorrectly(
+    "#state",
+    userData.address ? userData.address.state : undefined,
+    "#defaultStateOption"
+  );
+
+  //Dados de Contato
+  findAndUpdateValue("#responsible", userData.fullname);
+  findAndUpdateValue("#email", userData.email);
+  findAndUpdateValue("#phone", userData.phone);
+  findAndUpdateValue("#cellphone", userData.cellphone);
+
+  //Requisição de Análise
+  findAndUpdateValue(
+    "#reqCity",
+    userData.address ? userData.address.city : undefined
+  );
+  findAndSelectCorrectly(
+    "#reqState",
+    userData.address ? userData.address.state : undefined,
+    "#defaultStateReqOption"
+  );
 });
--- routes/users.js
+++ routes/users.js
@@ -1,315 +1,390 @@
-const express = require('express');
-var admin = require('firebase-admin');
+const express = require("express");
+var admin = require("firebase-admin");
 const router = express.Router();
-const auth = require('../middlewares/auth');
-const User = require('../models/user');
-const Email = require('../models/email');
+const auth = require("../middlewares/auth");
+const User = require("../models/user");
+const Email = require("../models/email");
 
 /* GET home page. */
-router.get('/', auth.isAuthenticated, function (req, res) {
-  User.getByQuery({ type: { $in: ["Gerencia", "Produtor"] }, deleted: false }).then((users) => {
-    res.render('admin/users/index', { title: 'Usuários', layout: 'layoutDashboard.hbs', users, ...req.session });
-
-  }).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  });
-
-});
-
-router.get('/pending', auth.isAuthenticated, function (req, res) {
-
-  User.getPendingAndInactive().then((result) => {
-    let [obj1, obj2] = result;
-
-    let inactives;
-    let pending;
-
-    if (obj1) {
-      if (obj1._id == "Inativo")
-        inactives = obj1.users;
-      else
-        pending = obj1.users;
-    }
-
-    if (obj2) {
-      if (obj2._id == "Inativo")
-        inactives = obj2.users;
-      else
-        pending = obj2.users;
-    }
-
-    res.render('admin/users/pending', { title: 'Usuários pendentes', layout: 'layoutDashboard.hbs', inactives, pending, ...req.session });
-  }).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  });
-
-});
-
-router.get('/associated', auth.isAuthenticated, function (req, res) {
-
-  User.getAll().then((users) => {
-    res.render('admin/users/associated', { title: 'Conveniados', layout: 'layoutDashboard.hbs', users, ...req.session });
-  }).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  });
-
-});
-
-router.get('/analysts', auth.isAuthenticated, function (req, res) {
-
-  User.getByQuery({ type: "Analista" }).then((users) => {
-    //console.log(users);
-    res.render('admin/users/analysts', { title: 'Produdores', layout: 'layoutDashboard.hbs', users, ...req.session });
-
-  }).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  });
-
-});
-
-router.get('/producers', auth.isAuthenticated, function (req, res) {
-
-  User.getAll().then((users) => {
-    res.render('admin/users/producers', { title: 'Produdores', layout: 'layoutDashboard.hbs', users, ...req.session });
-
-  }).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  });
-
-});
-
-router.get('/managers', auth.isAuthenticated, function (req, res) {
-
-  User.getByQuery({ type: "Gerencia" }).then((users) => {
-    const loggedID = req.session.user._id
-    res.render('admin/users/managers', { title: 'Gerentes', layout: 'layoutDashboard.hbs', users, ...req.session, loggedID });
-
-  }).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  });
-
-});
-
-router.get('/managers/:id', auth.isAuthenticated, function (req, res) {
-
-  User.getAssociatedMaganersById(req.params.id).then((users) => {
-    User.getById(req.params.id).then((user) => {
-      res.render('admin/users/managers', { title: 'Gerentes Associados', layout: 'layoutDashboard.hbs', users, user, ...req.session });
-    }).catch((error) => {
+router.get("/", auth.isAuthenticated, function (req, res) {
+  User.getByQuery({ type: { $in: ["Gerencia", "Produtor"] }, deleted: false })
+    .then((users) => {
+      res.render("admin/users/index", {
+        title: "Usuários",
+        layout: "layoutDashboard.hbs",
+        users,
+        ...req.session,
+      });
+    })
+    .catch((error) => {
       console.warn(error);
-      res.redirect('/error');
+      res.redirect("/error");
     });
-  }).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  });
-
 });
 
+router.get("/pending", auth.isAuthenticated, function (req, res) {
+  User.getPendingAndInactive()
+    .then((result) => {
+      let [obj1, obj2] = result;
 
-router.get('/producers/:id', auth.isAuthenticated, function (req, res) {
+      let inactives;
+      let pending;
 
-  User.getAssociatedProducersById(req.params.id).then((users) => {
-    //console.log(users);
-    User.getById(req.params.id).then((user) => {
-      //console.log(user);
-      res.render('admin/users/producers', { title: 'Produtores Associados', layout: 'layoutDashboard.hbs', users, user, ...req.session });
-    }).catch((error) => {
+      if (obj1) {
+        if (obj1._id == "Inativo") inactives = obj1.users;
+        else pending = obj1.users;
+      }
+
+      if (obj2) {
+        if (obj2._id == "Inativo") inactives = obj2.users;
+        else pending = obj2.users;
+      }
+
+      res.render("admin/users/pending", {
+        title: "Usuários pendentes",
+        layout: "layoutDashboard.hbs",
+        inactives,
+        pending,
+        ...req.session,
+      });
+    })
+    .catch((error) => {
       console.warn(error);
-      res.redirect('/error');
+      res.redirect("/error");
     });
-  }).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  });
-
 });
-router.post('/edit/:id', auth.isAuthenticated, function (req, res) {
+
+router.get("/associated", auth.isAuthenticated, function (req, res) {
+  User.getAll()
+    .then((users) => {
+      res.render("admin/users/associated", {
+        title: "Conveniados",
+        layout: "layoutDashboard.hbs",
+        users,
+        ...req.session,
+      });
+    })
+    .catch((error) => {
+      console.warn(error);
+      res.redirect("/error");
+    });
+});
+
+router.get("/analysts", auth.isAuthenticated, function (req, res) {
+  User.getByQuery({ type: "Analista" })
+    .then((users) => {
+      //console.log(users);
+      res.render("admin/users/analysts", {
+        title: "Produdores",
+        layout: "layoutDashboard.hbs",
+        users,
+        ...req.session,
+      });
+    })
+    .catch((error) => {
+      console.warn(error);
+      res.redirect("/error");
+    });
+});
+
+router.get("/producers", auth.isAuthenticated, function (req, res) {
+  User.getAll()
+    .then((users) => {
+      res.render("admin/users/producers", {
+        title: "Produdores",
+        layout: "layoutDashboard.hbs",
+        users,
+        ...req.session,
+      });
+    })
+    .catch((error) => {
+      console.warn(error);
+      res.redirect("/error");
+    });
+});
+
+router.get("/managers", auth.isAuthenticated, function (req, res) {
+  User.getByQuery({ type: "Gerencia" })
+    .then((users) => {
+      const loggedID = req.session.user._id;
+      res.render("admin/users/managers", {
+        title: "Gerentes",
+        layout: "layoutDashboard.hbs",
+        users,
+        ...req.session,
+        loggedID,
+      });
+    })
+    .catch((error) => {
+      console.warn(error);
+      res.redirect("/error");
+    });
+});
+
+router.get("/managers/:id", auth.isAuthenticated, function (req, res) {
+  User.getAssociatedMaganersById(req.params.id)
+    .then((users) => {
+      User.getById(req.params.id)
+        .then((user) => {
+          res.render("admin/users/managers", {
+            title: "Gerentes Associados",
+            layout: "layoutDashboard.hbs",
+            users,
+            user,
+            ...req.session,
+          });
+        })
+        .catch((error) => {
+          console.warn(error);
+          res.redirect("/error");
+        });
+    })
+    .catch((error) => {
+      console.warn(error);
+      res.redirect("/error");
+    });
+});
+
+router.get("/producers/:id", auth.isAuthenticated, function (req, res) {
+  User.getAssociatedProducersById(req.params.id)
+    .then((users) => {
+      //console.log(users);
+      User.getById(req.params.id)
+        .then((user) => {
+          //console.log(user);
+          res.render("admin/users/producers", {
+            title: "Produtores Associados",
+            layout: "layoutDashboard.hbs",
+            users,
+            user,
+            ...req.session,
+          });
+        })
+        .catch((error) => {
+          console.warn(error);
+          res.redirect("/error");
+        });
+    })
+    .catch((error) => {
+      console.warn(error);
+      res.redirect("/error");
+    });
+});
+router.post("/edit/:id", auth.isAuthenticated, function (req, res) {
   const { user } = req.body;
-  
-  if (user && user.type !== "Gerencia"){
+
+  if (user && user.type !== "Gerencia") {
     user.associatedProducers = [];
   }
 
-  User.update(req.params.id, user).then(() => {
-    req.flash('success', 'Usuário editado com sucesso.');
-    res.redirect(`/users/show/${req.params.id}/%20`);
-  }).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  });
-
+  User.update(req.params.id, user)
+    .then(() => {
+      req.flash("success", "Usuário editado com sucesso.");
+      res.redirect(`/users/show/${req.params.id}/%20`);
+    })
+    .catch((error) => {
+      console.warn(error);
+      res.redirect("/error");
+    });
 });
 
-router.get('/show/:id/:returnRoute', auth.isAuthenticated, async function (req, res) {
-  const { id } = req.params;
-  function existsInArray(element, array) {
-    array = array.map((user) => {
-      return user._id;
-    })
-    //console.log("Vetor de associados: ", array);
-    if (array.indexOf(element) === -1) {
-      //console.log(element, "nao pertence");
-      return false;
-    } else {
-      //console.log(element, " pertence");
-      return true;
+router.get(
+  "/show/:id/:returnRoute",
+  auth.isAuthenticated,
+  async function (req, res) {
+    const { id } = req.params;
+    function existsInArray(element, array) {
+      array = array.map((user) => {
+        return user._id;
+      });
+      //console.log("Vetor de associados: ", array);
+      if (array.indexOf(element) === -1) {
+        //console.log(element, "nao pertence");
+        return false;
+      } else {
+        //console.log(element, " pertence");
+        return true;
+      }
+    }
+
+    try {
+      const actualUser = await User.getByIdAndPopulate(id);
+      let producers = await User.getAllActiveProducers();
+
+      const associated = actualUser ? actualUser.associatedProducers : [];
+
+      associated.forEach((user) => {
+        user.actualUser = { _id: id };
+      });
+
+      const hasAssociated = associated.length > 0 ? true : false;
+
+      //Remove associated producers from the main list
+      producers = producers.filter(function (producer) {
+        if (existsInArray(producer._id, associated)) {
+          //console.log("Não passou ", producer.fullname);
+          return false;
+        }
+        if (producer._id == id) {
+          //console.log("Não passou ", producer.fullname);
+          return false;
+        }
+        //console.log("Passou ", producer.fullname);
+        return true;
+      });
+
+      const haveAvailable = producers.length > 0 ? true : false;
+
+      res.render("admin/users/show", {
+        title: "Perfil do usuário",
+        layout: "layoutDashboard.hbs",
+        returnRoute: req.params.returnRoute,
+        actualUser,
+        associated,
+        producers,
+        hasAssociated,
+        haveAvailable,
+        ...req.session,
+      });
+    } catch (error) {
+      console.warn(error);
+      res.redirect("/error");
     }
   }
+);
 
-  try {
-    const actualUser = await User.getByIdAndPopulate(id);
-    let producers = await User.getAllActiveProducers();
-
-    const associated = actualUser ? actualUser.associatedProducers : [];
-
-    associated.forEach((user) => {
-      user.actualUser = { _id: id };
+router.post(
+  "/approve/:id",
+  auth.isAuthenticated,
+  auth.isFromLab,
+  function (req, res) {
+    User.getById(req.params.id).then((user) => {
+      Email.userApprovedEmail(user.email, user.fullname.split(" ")[0]).catch(
+        (error) => {
+          req.flash(
+            "danger",
+            "Não foi possível enviar o email para o usuário aprovado."
+          );
+        }
+      );
     });
 
-    const hasAssociated = (associated.length > 0) ? true : false;
-
-    //Remove associated producers from the main list
-    producers = producers.filter(function (producer) {
-      if (existsInArray(producer._id, associated)) {
-        //console.log("Não passou ", producer.fullname);
-        return false;
-      }
-      if (producer._id == id) {
-        //console.log("Não passou ", producer.fullname);
-        return false;
-      }
-      //console.log("Passou ", producer.fullname);
-      return true;
-    })
-
-    const haveAvailable = (producers.length > 0) ? true : false;
-
-    res.render('admin/users/show', {
-      title: 'Perfil do usuário',
-      layout: 'layoutDashboard.hbs',
-      returnRoute: req.params.returnRoute,
-      actualUser,
-      associated,
-      producers,
-      hasAssociated,
-      haveAvailable,
-      ...req.session
-    });
-
-  } catch (error) {
-    console.warn(error);
-    res.redirect('/error');
-  };
-});
-
-router.post('/approve/:id', auth.isAuthenticated, auth.isFromLab, function (req, res) {
-  User.getById(req.params.id).then((user) => {
-    Email.userApprovedEmail(user.email, user.fullname.split(' ')[0]).catch((error) => {
-      req.flash('danger', 'Não foi possível enviar o email para o usuário aprovado.');
-    });
-  });
-
-  const user = {
-    status: 'Ativo'
-  };
-
-  User.update(req.params.id, user).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  }).then(() => {
-    req.flash('success', 'Usuário aprovado com sucesso.');
-    res.redirect('/users/pending');
-  });
-});
-
-
-router.post('/pending/:id', auth.isAuthenticated, function (req, res) {
-
-  const user = {
-    status: 'Aguardando aprovação'
-  };
-
-  User.update(req.params.id, user).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  }).then(() => {
-    req.flash('success', 'Usuário ativado com sucesso.');
-    res.redirect('/users/pending');
-  });
-});
-
-router.post('/reject/:id', auth.isAuthenticated, function (req, res) {
-  User.getById(req.params.id).then((user) => {
-    Email.userRejectedEmail(user.email, user.fullname).catch((error) => {
-      req.flash('danger', 'Não foi possível enviar o email para o usuário rejeitado.');
-    });
-  });
-  const user = {
-    status: 'Inativo'
-  };
-  User.update(req.params.id, user).catch((error) => {
-    console.warn(error);
-    res.redirect('/error');
-  }).then(() => {
-    req.flash('success', 'Usuário Inativado com sucesso.');
-    res.redirect('/users/pending');
-  });
-});
-
-router.post('/block/:id', auth.isAuthenticated, function (req, res) {
-  User.getById(req.params.id).then((user) => {
-    admin.auth().deleteUser(user.uid).then(function () {
-
-      Email.userRejectedEmail(user.email, user.fullname).catch((error) => {
-        req.flash('danger', 'Não foi possível enviar o email para o usuário rejeitado.');
-      });
-
-      User.delete(req.params.id).then(() => {
-        req.flash('success', 'Usuário deletado com sucesso.');
-        res.redirect('/users/pending');
-      }).catch((error) => {
-        res.redirect('/users/pending');
-      });
-
-    }).catch(function (error) {
-      console.log('Error deleting user:', error);
-      req.flash('danger', 'Error deleting user');
-      res.redirect('/users/pending');
-    });;
-
-  });
-
-});
-
-router.post('/approvepayment/:id', auth.isAuthenticated, function (req, res) {
-  User.getById(req.params.id).then((user) => {
-
-    const userUpdate = {
-      debt: !user.debt,
+    const user = {
+      status: "Ativo",
     };
 
-    let text = userUpdate.debt ? 'Pagamento reprovado com sucesso.' : 'Pagamento aprovado com sucesso.';
-    let type = userUpdate.debt ? 'danger' : 'success';
+    User.update(req.params.id, user)
+      .catch((error) => {
+        console.warn(error);
+        res.redirect("/error");
+      })
+      .then(() => {
+        req.flash("success", "Usuário aprovado com sucesso.");
+        res.redirect("/users/pending");
+      });
+  }
+);
 
-    User.update(req.params.id, userUpdate).then(() => {
-      req.flash(type, text);
-      res.redirect('/users');
-    }).catch((error) => {
+router.post("/pending/:id", auth.isAuthenticated, function (req, res) {
+  const user = {
+    status: "Aguardando aprovação",
+  };
+
+  User.update(req.params.id, user)
+    .catch((error) => {
       console.warn(error);
-      res.redirect('/error');
+      res.redirect("/error");
+    })
+    .then(() => {
+      req.flash("success", "Usuário ativado com sucesso.");
+      res.redirect("/users/pending");
     });
-  }).catch((error) => {
-    res.redirect('/error');
-    console.warn(error);
+});
+
+router.post("/reject/:id", auth.isAuthenticated, function (req, res) {
+  User.getById(req.params.id).then((user) => {
+    Email.userRejectedEmail(user.email, user.fullname).catch((error) => {
+      req.flash(
+        "danger",
+        "Não foi possível enviar o email para o usuário rejeitado."
+      );
+    });
+  });
+  const user = {
+    status: "Inativo",
+  };
+  User.update(req.params.id, user)
+    .catch((error) => {
+      console.warn(error);
+      res.redirect("/error");
+    })
+    .then(() => {
+      req.flash("success", "Usuário Inativado com sucesso.");
+      res.redirect("/users/pending");
+    });
+});
+
+router.post("/block/:id", auth.isAuthenticated, function (req, res) {
+  User.getById(req.params.id).then((user) => {
+    admin
+      .auth()
+      .deleteUser(user.uid)
+      .then(function () {
+        Email.userRejectedEmail(user.email, user.fullname).catch((error) => {
+          req.flash(
+            "danger",
+            "Não foi possível enviar o email para o usuário rejeitado."
+          );
+        });
+
+        User.delete(req.params.id)
+          .then(() => {
+            req.flash("success", "Usuário deletado com sucesso.");
+            res.redirect("/users/pending");
+          })
+          .catch((error) => {
+            res.redirect("/users/pending");
+          });
+      })
+      .catch(function (error) {
+        console.log("Error deleting user:", error);
+        req.flash("danger", "Error deleting user");
+        res.redirect("/users/pending");
+      });
   });
 });
 
+router.post("/approvepayment/:id", auth.isAuthenticated, function (req, res) {
+  User.getById(req.params.id)
+    .then((user) => {
+      const userUpdate = {
+        debt: !user.debt,
+      };
+
+      let text = userUpdate.debt
+        ? "Pagamento reprovado com sucesso."
+        : "Pagamento aprovado com sucesso.";
+      let type = userUpdate.debt ? "danger" : "success";
+
+      User.update(req.params.id, userUpdate)
+        .then(() => {
+          req.flash(type, text);
+          res.redirect("/users");
+        })
+        .catch((error) => {
+          console.warn(error);
+          res.redirect("/error");
+        });
+    })
+    .catch((error) => {
+      res.redirect("/error");
+      console.warn(error);
+    });
+});
+
+router.get("/byid/:_id", auth.isAuthenticated, async function (req, res) {
+  const user = await User.getById(req.params._id);
+  return res.status(200).json(user);
+});
 
 module.exports = router;
